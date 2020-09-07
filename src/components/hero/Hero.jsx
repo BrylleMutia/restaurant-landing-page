e221bcba9c378757159f9592f051a91d91b25b94
Brylle Mutia <bryllemutia3@gmail.com>
2020-09-07 15:34:39 +0800

    fixed responsiveness for hero and navbar components

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { hero, hero__title, hero__header, hero__subheader, hero__form } from "./Hero.module.css";
-import { container, vertical_spacer, input, button, horizontal_spacer } from "../../App.module.css";
+import { container, vertical_spacer, input, button, side_padding } from "../../App.module.css";
 
 import cx from "classnames";
 import Navbar from "./navbar/Navbar";
@@ -11,14 +11,25 @@ const Hero = () => {
             <div className={hero}>
                 <div className={container}>
                     <Navbar />
+                    <div className={side_padding}>
                         <div className={cx(hero__title, vertical_spacer)}>
-                            <h3 className={hero__header}>The Best Restaurant in Town</h3>
+                            <h3 className={hero__header}>Food for Every Craving</h3>
                             <p className={hero__subheader}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, rem?</p>
                             <form className={hero__form}>
-                                <input style={{ borderBottomRightRadius: "0", borderTopRightRadius: "0" }} className={input} type="search" name="search" id="search" placeholder="Search our menu" />
-                                <button style={{ borderBottomLeftRadius: "0", borderTopLeftRadius: "0" }} className={button} type="submit">Search</button>
+                                <input
+                                    style={{ borderBottomRightRadius: "0", borderTopRightRadius: "0" }}
+                                    className={input}
+                                    type="search"
+                                    name="search"
+                                    id="search"
+                                    placeholder="Search our menu"
+                                />
+                                <button style={{ borderBottomLeftRadius: "0", borderTopLeftRadius: "0" }} className={button} type="submit">
+                                    Search
+                                </button>
                             </form>
                         </div>
+                    </div>
                 </div>
             </div>
         </header>
--- src/components/hero/navbar/Navbar.jsx
+++ src/components/hero/navbar/Navbar.jsx
@@ -1,14 +1,25 @@
-import React from "react";
-import { navbar, brand, navbar__links, navbar__link } from "./Navbar.module.css";
-import { container, horizontal_spacer } from "../../../App.module.css";
+import React, { useState } from "react";
+import { navbar, brand, navbar__links, navbar__link, navbar__div, menu, menu__hamburger, menu__open } from "./Navbar.module.css";
+import { horizontal_spacer } from "../../../App.module.css";
 import cx from "classnames";
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleOpenMenu = () => {
+        setIsOpen((prev) => !prev);
+    };
+
     return (
         <nav>
-            <div className={navbar}>
-                <h3 className={brand}>Totsuki Academy</h3>
-                <ul className={cx(navbar__links, horizontal_spacer)}>
+            <div className={navbar} style={{ height: isOpen && "11.6em" }}>
+                <div className={navbar__div}>
+                    <h3 className={brand}>Totsuki Academy</h3>
+                    <div className={cx(menu, isOpen && menu__open)} onClick={handleOpenMenu}>
+                        <div className={menu__hamburger}></div>
+                    </div>
+                </div>
+                <ul className={cx(navbar__links, horizontal_spacer)} style={{ opacity: isOpen || window.innerWidth > 740 ? "1" : "0" }}>
                     <li className={navbar__link}>
                         <a href="#">About Us</a>
                     </li>
