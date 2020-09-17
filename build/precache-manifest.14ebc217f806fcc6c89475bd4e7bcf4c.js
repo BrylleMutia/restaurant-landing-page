bfe3b233ed1bd08879ac7ae1c73d09f522d76c5d
Brylle Mutia <bryllemutia3@gmail.com>
2020-09-18 03:23:25 +0800

    initial commit upon build

---
+++ build/precache-manifest.14ebc217f806fcc6c89475bd4e7bcf4c.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "556a18a41faf287ea01e4e7028559e22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14baf8c2aab16fe07d1a",
+    "url": "/static/css/main.723a9ea4.chunk.css"
+  },
+  {
+    "revision": "ee7f5baf13b9fa7b7126",
+    "url": "/static/js/2.deeae5b9.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/static/js/2.deeae5b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14baf8c2aab16fe07d1a",
+    "url": "/static/js/main.946a20b1.chunk.js"
+  },
+  {
+    "revision": "a03177b6ed9630ce53d6",
+    "url": "/static/js/runtime-main.4560eeac.js"
+  },
+  {
+    "revision": "84db200c3114fdc383b34794fad53099",
+    "url": "/static/media/amazon.84db200c.png"
+  },
+  {
+    "revision": "083eeb18103807df31f1de90e8e45911",
+    "url": "/static/media/app.083eeb18.png"
+  },
+  {
+    "revision": "09afc88dca425f9d667d8e90a30b4ae5",
+    "url": "/static/media/appstore.09afc88d.png"
+  },
+  {
+    "revision": "1398cf1cfd70c936311a4611201c4ec5",
+    "url": "/static/media/cocacola.1398cf1c.png"
+  },
+  {
+    "revision": "f8e42a1013ee81f6c5208add98ec8511",
+    "url": "/static/media/download-1.f8e42a10.jpg"
+  },
+  {
+    "revision": "563771ebbca4f4a59431cc7ea24765d0",
+    "url": "/static/media/featured-1.563771eb.jpg"
+  },
+  {
+    "revision": "76400b9e9a515ab018f7e47aaa936817",
+    "url": "/static/media/featured-2.76400b9e.jpg"
+  },
+  {
+    "revision": "9a666920b514aa61e63a3a6456d067e9",
+    "url": "/static/media/featured-3.9a666920.jpg"
+  },
+  {
+    "revision": "9758f479cad7c50864a5c8017e1cc9b3",
+    "url": "/static/media/featured-4.9758f479.jpg"
+  },
+  {
+    "revision": "57ce63b6024ef68bf551d416b984970f",
+    "url": "/static/media/featured-5.57ce63b6.jpg"
+  },
+  {
+    "revision": "04eb4b737d93ab2ed178e0b54dca88af",
+    "url": "/static/media/featured-6.04eb4b73.jpg"
+  },
+  {
+    "revision": "5492a2ffefb24b91d71c9b9bcb41af63",
+    "url": "/static/media/featured-7.5492a2ff.jpg"
+  },
+  {
+    "revision": "579b96e82e5df924d455040040ec3609",
+    "url": "/static/media/grid-2.579b96e8.jpg"
+  },
+  {
+    "revision": "bcfc69e718166696c113fc8fa94cc7f8",
+    "url": "/static/media/grid-3.bcfc69e7.jpg"
+  },
+  {
+    "revision": "88da6bce8de3ffc2a932a8447b64462d",
+    "url": "/static/media/grid-4.88da6bce.jpg"
+  },
+  {
+    "revision": "71d571c79e33888d8a8f10b718542cc2",
+    "url": "/static/media/hero-1.71d571c7.jpg"
+  },
+  {
+    "revision": "ec7376fd14d9a6e023acd87002b45093",
+    "url": "/static/media/instagram.ec7376fd.png"
+  },
+  {
+    "revision": "cd839006923d3a4bffe2c389e4f2d5ae",
+    "url": "/static/media/playstore.cd839006.png"
+  }
+]);
