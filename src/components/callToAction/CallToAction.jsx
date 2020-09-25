6c10999335be0c6070b2fab4928ef62cee3cef9f
Brylle Mutia <bryllemutia3@gmail.com>
2020-09-25 08:41:59 +0800

    fixed responsiveness on call to action form

--- src/components/callToAction/CallToAction.jsx
+++ src/components/callToAction/CallToAction.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { action, action__form, action__text } from "./CallToAction.module.css";
-import { container, side_padding, flex_row, vertical_spacer, button } from "../../App.module.css";
+import { container, side_padding, flex_row, vertical_spacer, button, input } from "../../App.module.css";
 
 import cx from "classnames";
 
@@ -13,9 +13,9 @@ const CallToAction = () => {
                         <h3>Book a reservation now!</h3>
                         <p> We'll ask for details through email.</p>
                     </div>
-                    <form className={action__form} action="POST">
-                        <input type="email" name="email" id="email" placeholder="Enter your email" />
-                        <button style={{ "--button-radius": "0" }} className={button} type="submit">add reservation</button>
+                    <form className={action__form} style={{ "--button-radius": "0", "--input-radius": "0" }} action="POST">
+                        <input className={input} type="email" name="email" id="email" placeholder="Enter your email" />
+                        <button className={button} type="submit">add reservation</button>
                     </form>
                 </div>
             </div>
